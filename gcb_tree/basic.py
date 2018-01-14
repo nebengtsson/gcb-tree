d75e2dd1328393e88c66773f4579b961ea27e642
Nils Bengtsson <nebengtsson@gmail.com>
2018-01-14 22:34:56 +0100

    Refactor name, smal ->small

--- gcb_tree/basic.py
+++ gcb_tree/basic.py
@@ -6,13 +6,12 @@ arange = np.arange
 def biggest_bit(a):
     return 1 << a.bit_length()-1
 
-
-def smalest_bit(a):
+def smallest_bit(a):
     return (1 + (a ^ (a-1))) >> 1
 
 
 def node_range(value):
-    sb = smalest_bit(value)
+    sb = smallest_bit(value)
     return (value-sb, value+sb-1)
 
 
@@ -67,62 +66,62 @@ class Node():
                  "parent", "big_child", "smal_child",
                  "range_smal", "range_big"]
 
-    def __init__(self, value, child_type, parent, big_child, smal_child):
+    def __init__(self, value, child_type, parent, big_child, small_child):
         #self.type = 'node'
         self.value = value
         self.child_type = child_type
         self.parent = parent
         self.big_child = big_child
-        self.smal_child = smal_child
+        self.small_child = small_child
 
         # Pre-calc node range
         nr = node_range(value)
-        self.range_smal = nr[0]
+        self.range_small = nr[0]
         self.range_big = nr[1]
 
     def add(self, value):
         if self._test_value(value):
             if value < self.value:
-                return self.smal_child.search_node_down(value)
-            return self.big_child.search_node_down(value)
+                return self.small_child.add(value)
+            return self.big_child.add(value)
         else:
             return self.insert_new_node_above(value)
 
     def get(self, value):
         if value < self.value:
-            return self.smal_child.get(value)
+            return self.small_child.get(value)
         return self.big_child.get(value)
 
     def insert_new_node_above(self, value):
         new_leaf = Leaf(value)
         if value < self.value:
             big_child = self
-            smal_child = new_leaf
+            small_child = new_leaf
         else:
             big_child = new_leaf
-            smal_child = self
+            small_child = self
 
         new_node = Node(
             find_value_node_value(value, self.value),
             self.child_type,
             self.parent,
             big_child,  # big
-            smal_child,  # smal
+            small_child,  # small
         )
 
-        if self.child_type == 'smal':
-            self.parent.smal_child = new_node
+        if self.child_type == 'small':
+            self.parent.small_child = new_node
         else:
             self.parent.big_child = new_node
         big_child.parent = new_node
-        smal_child.parent = new_node
+        small_child.parent = new_node
         big_child.child_type = 'big'
-        smal_child.child_type = 'smal'
+        small_child.child_type = 'small'
 
         return new_leaf
 
     def _test_value(self, value):
-        return self.range_smal <= value and value <= self.range_big
+        return self.range_small <= value and value <= self.range_big
 
 
 class Leaf(Node):
@@ -153,7 +152,7 @@ class Root(Node):
         self.child = None
         self.value = max_value / 2  # This is ambiguis but neede to use Node-functions
 
-        self.range_smal = 0
+        self.range_small = 0
         self.range_big = max_value
 
     def get(self, value):
@@ -162,14 +161,14 @@ class Root(Node):
     def add(self, value):
         if self._test_value(value):
             return self.child.add(value)
-        raise ValueError('Value not in Tree-range. ({self.range_smal} to {self.range_big})')
+        raise ValueError('Value not in Tree-range. ({self.range_small} to {self.range_big})')
 
     @property
-    def smal_child(self):
+    def small_child(self):
         return self.child
 
-    @smal_child.setter
-    def smal_child(self, node):
+    @small_child.setter
+    def small_child(self, node):
         self.child = node
 
     @property
